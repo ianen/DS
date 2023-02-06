5922e0b7ae5421858e792e050c0fce544afcadc5
ianen <3282068971@qq.com>
2023-02-06 17:33:46 +0800

    第四次修改

--- 栈与队列/源.cpp
+++ 栈与队列/源.cpp
@@ -1,6 +1,7 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 //函数结果状态代码
 #define OK    1
@@ -184,6 +185,133 @@ Status Pop3(LinkStack* S, SElemType* e)
 	return OK;
 }
 
+//————循环队列————
+//循环队列的顺序结构代码
+typedef int QElemType;
+typedef struct
+{
+	QElemType data[MAXSIZE];
+	int front;//头指针，指向队列的第一个元素
+	int rear;//尾指针，若队列不空，指向队列尾元素的下一个位置
+}SqQueue;
+
+/**
+  *@brief  初始化一个空队列Q
+  *@param  Q，要初始化的队列指针
+  *@retval 函数初始化队列状态结果，成功返回1
+  */
+Status Queue1Init(SqQueue** Q)
+{
+	*Q = (SqQueue*)malloc(sizeof(SqQueue));
+	(*Q)->front = 0;
+	(*Q)->rear = 0;
+	return OK;
+}
+
+/**
+  *@brief  求队列Q的长度
+  *@param  Q，队列
+  *@retval 队列的长度
+  */
+int Queue1Length(SqQueue Q)
+{
+	return (Q.rear - Q.front + MAXSIZE) % MAXSIZE;
+}
+
+/**
+  *@brief  将元素e插入队列Q的尾部
+  *@param  Q，要插入队列的指针
+  *@param e，要插入的元素
+  *@retval 函数插入元素状态结果，成功返回1，失败则返回0
+  */
+Status EnQueue1(SqQueue* Q, QElemType e)
+{
+	if ((Q->rear + 1) % MAXSIZE == Q->front)//队列满时
+		return ERROR;
+	Q->data[Q->rear] = e;
+	Q->rear = (Q->rear + 1) % MAXSIZE;
+	return OK;
+}
+
+/**
+  *@brief  若队列不空，则删除队列的头元素，用e返回其值
+  *@param  Q，要删除元素队列的指针
+  *@param e，返回删除元素的指针
+  *@retval 函数删除元素状态结果，成功返回1，失败则返回0
+  */
+Status DeQueue1(SqQueue* Q, QElemType* e)
+{
+	if (Q->front == Q->rear)//队列为空时
+		return ERROR;
+	*e = Q->data[Q->front];
+	Q->front = (Q->front + 1) % MAXSIZE;
+	return OK;
+}
+
+//————队列的链式存储结构————
+//链队列结构代码
+typedef struct QNode
+{
+	QElemType data;
+	struct QNode* next;
+}QNode,*QueuePtr;
+
+typedef struct
+{
+	QueuePtr front, rear;
+}LinkQueue;
+
+/**
+  *@brief  初始化一个空链队列Q
+  *@param  Q，要初始化的队列指针
+  *@retval 函数初始化队列状态结果，成功返回1
+  */
+Status Queue2Init(LinkQueue** Q)
+{
+	*Q = (LinkQueue*)malloc(sizeof(LinkQueue));
+	(*Q)->front = (QueuePtr)malloc(sizeof(QNode));
+	(*Q)->rear = (*Q)->front;
+	return OK;
+}
+
+/**
+  *@brief  将元素e插入链队列Q的尾部
+  *@param  Q，要插入链队列的指针
+  *@param e，要插入的元素
+  *@retval 函数插入元素状态结果，成功返回1，失败则返回0
+  */
+Status EnQueue2(LinkQueue* Q, QElemType e)
+{
+	QueuePtr s = (QueuePtr)malloc(sizeof(QNode));
+	if (!s)//存储分配失败
+		exit(OVERFLOW);
+	s->data = e;
+	s->next = NULL;
+	Q->rear->next = s;
+	Q->rear = s;
+	return OK;
+}
+
+/**
+  *@brief  若队列不空，则删除队列的头元素，用e返回其值
+  *@param  Q，要删除元素队列的指针
+  *@param e，返回删除元素的指针
+  *@retval 函数删除元素状态结果，成功返回1，失败则返回0
+  */
+Status DeQueue2(LinkQueue* Q, QElemType* e)
+{
+	QueuePtr p;
+	if (Q->front == Q->rear)//队列为空时
+		return ERROR;
+	p = Q->front->next;
+	*e = p->data;
+	Q->front->next = p->next;
+	if (Q->rear == p)//当删除后队列为空时
+		Q->rear = Q->front;
+	free(p);
+	return OK;
+}
+
 int main()
 {
 	//栈的顺序存储结构测试
@@ -228,5 +356,32 @@ int main()
 	Pop3(P, e);
 	printf("%d ", *e);
 	Pop3(P, e);
+	printf("%d \n", *e);
+
+	//循环队列的顺序结构测试
+	SqQueue* Q;
+	Queue1Init(&Q);
+	EnQueue1(Q, 100);
+	EnQueue1(Q, 200);
+	EnQueue1(Q, 300);
+	printf("循环队列的顺序结构测试结果：长度%d ", Queue1Length(*Q));
+	DeQueue1(Q, e);
+	printf("%d ", *e);
+	DeQueue1(Q, e);
+	printf("%d ", *e);
+	DeQueue1(Q, e);
+	printf("%d \n", *e);
+
+	//队列的链式存储结构测试
+	LinkQueue *LQ;
+	Queue2Init(&LQ);
+	EnQueue2(LQ, 100);
+	EnQueue2(LQ, 200);
+	EnQueue2(LQ, 300);
+	DeQueue2(LQ, e);
+	printf("队列的链式存储结构测试结果：%d ", *e);
+	DeQueue2(LQ, e);
+	printf("%d ", *e);
+	DeQueue2(LQ, e);
 	printf("%d \n", *e);
 }
